77317d1e1984c46056915e104e28c71f0b6411bc
xiannvrong <1796940658@qq.com>
2019-06-19 15:15:02 +0800

    xx

--- pages/scanlist/scanlist.vue
+++ pages/scanlist/scanlist.vue
@@ -1,15 +1,15 @@
 <template>
 	<view class="wrapper">
 		<view class="left_area">
-			<!-- 分隔器 -->
+			<view class="placeholder"></view>
 			<view class="segmented_control_area">
 				<uni-segmented-control :current="current" :values="items" @clickItem="onClickItem" style-type="button" active-color="#4A4A4A"></uni-segmented-control>
-				<view class="content">
-					<view v-show="current === 0">选项卡1的内容</view>
-					<view v-show="current === 1">选项卡2的内容</view>
-				</view>
 			</view>
 			<view class="clear_area">清空</view>
+		</view>
+		<view class="content">
+			<view v-show="current === 0">选项卡1的内容选项卡1的内容选项卡1的内容选项卡1的内容选项卡1的内容选项卡1的内容选项卡1的内容选项卡1的内容</view>
+			<view v-show="current === 1">选项卡2的内容</view>
 		</view>
 	</view>
 </template>
@@ -42,17 +42,24 @@ export default {
 }
 .content {
 	width: 100%;
+	padding: 0;
 }
 .left_area {
 	display: flex;
 	flex-direction: row;
-	align-items: center;
+	justify-content: space-between;
 }
 .segmented_control_area {
-	width: 80%;
+	flex: 1;
 }
 .clear_area{
+	width: 60upx;
+	height: 42upx;
 	color: #000;
 	font-size: 30upx;
+	margin: 15upx 30upx 0 0;
+}
+.placeholder{
+	margin-left: 95upx;
 }
 </style>
