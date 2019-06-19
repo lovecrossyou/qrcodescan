98ca8ab0e2e11308471c17b794a10fe6ffdcf597
xiannvrong <1796940658@qq.com>
2019-06-19 14:05:02 +0800

    首页

---
+++ pages/gen-qrcode/components/operationItem.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="wrapper">
+		<image :src="describeIcon" class="describe_icon"></image>
+		<view class="describe_text">{{describeText}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		props:{
+			describeIcon:String,
+			describeText:String
+		}
+	}
+</script>
+
+<style scoped>
+	.wrapper{
+		width: 330upx;
+		height: 330upx;
+		background-color: #fff;
+		border-radius: 20upx;
+		padding: 48upx 0;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.describe_icon{
+		width: 140upx;
+		height: 140upx;
+	}
+	.describe_text{
+		color: #333333;
+		font-size: 34upx;
+		font-weight: 400;
+	}
+</style>
